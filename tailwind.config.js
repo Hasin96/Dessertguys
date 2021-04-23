382f91ba07e58455692f8558dd437c7a4b51a39c
Hasin Ajaz <hasin.ajaz1996@hotmail.com>
2021-04-23 16:11:28 +0100

    finished phone version of header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,26 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'phone': '0px',
+      'medium-mobile': '375px',
+      'large-mobile': '426px',
+      'tablet': '780px',
+      'laptop': '1200px',
+      'desktop': '1480px',
+      'large-desktop': '2560px'
+    },
     extend: {
       colors: {
         brand:"#b99656",
         "brand-border": "#aa8746",
         "brand-light": "#fffaeb",
+        "brand-lighter": "#FFA500",
         "neutral-header": "#7E7E7E",
         "neutral-text": "#7e7e7e",
-        "background-light" : "#f7f7f7"
+        "background-light" : "#f7f7f7",
+        "custom" : "#E1E1E1",
+        "icon-hover": "#333333"
       },
       spacing: {
         116: '32rem',
@@ -22,10 +34,10 @@ module.exports = {
       },
       letterSpacing: {
         header: '.3rem',
+        tabletHeader: '.1rem',
         subHeader: '.1rem',
         text: '.04rem'
        }
-     
     },
   },
   variants: {
